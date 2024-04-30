b49aabccb4efcdc7c5867fea5487e2bae3e0f596
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-04-30 21:48:29 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { videos } from '../../assets/data';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public movies = videos;
   constructor() {}
-
 }
